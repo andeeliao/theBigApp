a607a05f152c20661329032d559cf769620173c8
Andee Liao <xueqi.liao@gmail.com>
2019-05-21 17:21:00 -0700

    Initial Commit

---
+++ PocketKevinLauTests/PocketKevinLauTests.swift
@@ -0,0 +1,34 @@
+//
+//  PocketKevinLauTests.swift
+//  PocketKevinLauTests
+//
+//  Created by xue qi liao on 5/21/19.
+//  Copyright © 2019 xue qi liao. All rights reserved.
+//
+
+import XCTest
+@testable import PocketKevinLau
+
+class PocketKevinLauTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
